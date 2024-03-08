222379f8e7153415814122f874d7193e3a922f0a
AkkiSeven <akshaymadhira@gmail.com>
2024-03-08 12:30:51 +0530

    Updated changes

--- wordle.py
+++ wordle.py
@@ -10,22 +10,23 @@ with open('words.json', 'r') as file:
 words = data['words']
 
 word = random.choice(words)
-print(word)
 
 if len(list(word)) != 5:
     print("Sorry, there was an error. Please run the program again")
 
-word = list(word.upper())
+word_list = list(word.upper())
+counter = 6
 
-print("Welcome to wordle. Enter a 5 letter word.")
-
+print("\nWelcome to wordle. Enter a 5 letter word.")
 
 while True:
-
-
-    your_try = input("Enter a 5 letter word: ")
-
+    if counter == 0:
+        print("Your chances are over. The correct word was " + word)
+        print("Please run the program to play again")
+        quit()
+    your_try = input(f"Enter a 5 letter word(You have {counter} chances left): ")
     your_try_list = list(your_try.upper())
+
     if len(your_try_list) != 5:
         print("\nLength of word should be 5 letters! Please try again...\n")
         continue
@@ -33,11 +34,9 @@ while True:
         print("\nPlease enter a valid 5 letter word.\n")
         continue
 
+    compare.compare_words(word_list, your_try_list)
+    counter -= 1
 
-
-    compare.compare_words(word, your_try_list)
-        
-
-    if word == your_try_list:
+    if word_list == your_try_list:
         print("You won the game!!! Congrats.")
         quit()
